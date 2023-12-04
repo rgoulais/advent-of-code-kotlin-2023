38e7de681f3702994fd9d84b2f0ca8d09caca100
Raphael Goulais <raphael.goulais@f5c.fr>
2023-12-04 06:58:05 +0100

    feat: Day 04

---
+++ src/Day04.kt
@@ -0,0 +1,45 @@
+import kotlin.math.pow
+
+fun main() {
+
+    fun parseGame(game: String) = game.split("|").map { s ->
+        s.trim().replace("\\s+".toRegex(), " ").split(" ")
+    }
+
+    fun getGameData(line: String): Pair<Int, List<List<String>>> {
+        val (gameHeaders, gameBody) = line.split(":")
+        val gameData = gameHeaders.split(" ")
+        return gameData.last().toInt() to parseGame(gameBody)
+    }
+
+    fun getNumMatch(wins: List<String>, game: List<String>) = wins.toSet().intersect(game.toSet()).size
+
+    fun part1(input: List<String>): Int {
+        var total = 0
+        for (line in input) {
+            val (_, gameData) = getGameData(line)
+            total += 2.0.pow(getNumMatch(gameData[0], gameData[1]).toDouble() - 1).toInt()
+        }
+        return total
+    }
+
+    fun part2(input: List<String>): Int {
+        val cards = List(input.size) { 1 }.toMutableList()
+        for (i in input.indices) {
+            val (_, gameData) = getGameData(input[i])
+            for (j in 1..getNumMatch(gameData[0], gameData[1])) {
+                cards[i + j] += cards[i]
+            }
+        }
+        return cards.sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
